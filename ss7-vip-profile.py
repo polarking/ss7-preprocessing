22cd46f4f5aad4a18c4db2cf97ce5c61b97112cd
Kristoffer Jensen <jensenkris91@gmail.com>
2016-04-12 18:48:04 +0200

    Add imsi to method header

--- ss7-vip-profile.py
+++ ss7-vip-profile.py
@@ -1,12 +1,10 @@
 import sys, csv, os
 
-def user_profile(csv_file_path):
+def user_profile(in_file_path, out_file_path, imsi):
   in_file = open(in_file_path, 'rb')
   out_file = open(out_file_path, 'wb')
 
   csv_reader = csv.reader(in_file, delimiter=',', quotechat='"')
   csv_writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-  header = "no,time_epoch,opc,dpc,length,map.message,sccp.calling.digits,sccp.calling.ssn,sccp.called.digits,sccp.called.ssn,imsi,msisdn,new_area,lac"
-
-  vip_imsi = "242"
+  #header = "no,time_epoch,opc,dpc,length,map.message,sccp.calling.digits,sccp.calling.ssn,sccp.called.digits,sccp.called.ssn,imsi,msisdn,new_area,lac"
